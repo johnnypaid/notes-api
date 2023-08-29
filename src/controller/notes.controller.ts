2320c14f7aa12efd060736f91f4c68b22099b041
johnnypaid <johnnypaid12@gmail.com>
2023-08-29 20:42:47 +0700

    api update

--- src/controller/notes.controller.ts
+++ src/controller/notes.controller.ts
@@ -24,6 +24,7 @@ export const CreateNotes: RequestHandler = async (
 ) => {
   const title = req.body.title;
   const text = req.body.text;
+
   try {
     if (!title) {
       throw createHttpError(400, "Note must have a title.");
@@ -57,6 +58,64 @@ export const GetNote: RequestHandler = async (
     }
 
     res.status(200).json(findNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const UpdateNote: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const title = req.body.title;
+  const text = req.body.text;
+
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+
+    if (!title) {
+      throw createHttpError(400, "Note must have a title.");
+    }
+
+    const findNote = await NoteModel.findById(req.params.id).exec();
+
+    if (!findNote) {
+      throw createHttpError(404, "Notes not found");
+    }
+
+    findNote.title = title;
+    findNote.text = text;
+
+    const updateNote = await findNote.save();
+
+    res.status(200).json(updateNote);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const DeleteNote: RequestHandler = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      throw createHttpError(400, "Invalid note id");
+    }
+
+    const findNote = await NoteModel.findById(req.params.id).exec();
+
+    if (!findNote) {
+      throw createHttpError(404, "Notes not found");
+    }
+
+    await findNote.deleteOne();
+
+    res.sendStatus(204);
   } catch (error) {
     next(error);
   }
--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
-import { CreateNotes, GetNote, GetNotes } from "../controller/notes.controller";
+import {
+  CreateNotes,
+  DeleteNote,
+  GetNote,
+  GetNotes,
+  UpdateNote,
+} from "../controller/notes.controller";
 
 // notes api
 export const routes = (router: Router) => {
   router.get("/api/notes", GetNotes);
   router.post("/api/notes", CreateNotes);
   router.get("/api/notes/:id", GetNote);
+  router.put("/api/notes/:id", UpdateNote);
+  router.delete("/api/notes/:id", DeleteNote);
 };
